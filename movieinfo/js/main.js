6c0c85333a4677b2438e45cace75e972eaaa59bf
Vishang Shah <vishangshah@users.noreply.github.com>
2018-06-19 16:31:18 -0400

    Update main.js

--- movieinfo/js/main.js
+++ movieinfo/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
 
 function getMovies(searchText)
 {
-    axios.get('http://www.omdbapi.com?t='+searchText)
+    axios.get('https://www.omdbapi.com?t='+searchText)
         .then((response) => {
             console.log(response);
         })
