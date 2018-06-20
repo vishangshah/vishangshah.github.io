d8f33fdce5166a093016119a790885ad5d57abfa
Vishang Shah <vishangshah@users.noreply.github.com>
2018-06-20 15:43:49 -0400

    Added page for movie details

--- movieinfo/js/main.js
+++ movieinfo/js/main.js
@@ -8,10 +8,72 @@ $(document).ready(() => {
 
 function getMovies(searchText)
 {
-    omdb_apikey = 'xxxxxxxx'
-    axios.get('https://www.omdbapi.com?t='+searchText+'&apikey='+omdb_apikey)
+    omdb_apikey = 'xxxxxx'
+    axios.get('https://www.omdbapi.com?s='+searchText+'&apikey='+omdb_apikey)
         .then((response) => {
-            console.log(response);
+            let movies = response.data.Search;
+            let output = '';
+            $.each(movies, (index, movie) => {
+                output += `
+                    <div class="col-md-3">
+                        <div class="well text-center">
+                            <img src="${movie.Poster}">
+                            <h5>${movie.Title}</h5>
+                            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                        </div>
+                    </div>
+                `;
+            });
+            
+            $('#movies').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function movieSelected(id)
+{
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie()
+{
+    let movieId = sessionStorage.getItem('movieId');
+    omdb_apikey = 'xxxxxx'
+    axios.get('https://www.omdbapi.com?i='+movieId+'&apikey='+omdb_apikey)
+        .then((response) => {
+            let movie = response.data;
+            let output = `
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="${movie.Poster}" class="thumbnail">
+                    </div>
+                    <div class="col-md-8">
+                        <h2>${movie.Title}</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Genre: </strong>${movie.Genre}</li>
+                            <li class="list-group-item"><strong>Released: </strong>${movie.Released}</li>
+                            <li class="list-group-item"><strong>Rated: </strong>${movie.Rated}</li>
+                            <li class="list-group-item"><strong>IMDB Rating: </strong>${movie.imdbRating}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="well">
+                        <hr>
+                        <h3>Plot</h3>
+                        ${movie.Plot}
+                        <hr>
+                        <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">IMDB</a>
+                        <a href="index.html" class="btn btn-default">Back to Search</a>
+                    </div>
+                </div>
+            `;
+
+        $("#movie").html(output);
         })
         .catch((err) => {
             console.log(err);
